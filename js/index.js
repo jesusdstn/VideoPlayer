d5809db467ef6196ab1cc1119687402761fc3e8d
Jesus Solis <53708771+jesusdstn@users.noreply.github.com>
2021-09-04 02:12:23 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const $video = document.getElementById("video");
+const $play = document.getElementById("play");
+const $pause = document.getElementById("pause");
+const $backward = document.getElementById("backward");
+const $forward = document.getElementById("forward");
+const $range = document.querySelector("#range");
+
+
+$play.addEventListener('click', playVideo);
+$pause.addEventListener('click', pauseVideo);
+
+function playVideo() {
+  $video.play();
+  $play.hidden = true;
+  $pause.hidden = false;
+}
+
+function pauseVideo() {
+  $video.pause();
+  $play.hidden = false;
+  $pause.hidden = true;
+}
+
+$backward.addEventListener('click', backwardVideo);
+
+function backwardVideo(){
+  $video.currentTime -= 10;
+}
+
+$forward.addEventListener('click', forwardVideo);
+function forwardVideo(){
+  $video.currentTime += 10;
+}
+
+$video.addEventListener('loadedmetadata',handleLoaderMetadata);
+$video.addEventListener('timeupdate', handleTimeUpdate);
+
+function handleLoaderMetadata(){
+  $range.max = $video.duration;
+}
+
+function handleTimeUpdate(){
+  $range.value = $video.currentTime;
+}
+
+$range.addEventListener('input',handleInput);
+function handleInput(){
+  $video.currentTime = $range.value;
+}
